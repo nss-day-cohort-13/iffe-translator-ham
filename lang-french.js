e75cd8e4bc6a024193bc1bff977db2885b68accd
Hannah Hall <hannahkhall13@gmail.com>
2016-04-26 14:04:57 -0500

    created french iife

---
+++ lang-french.js
@@ -0,0 +1,36 @@
+var Translator= (function() {
+	var french= {
+		a: "un",
+		an: "un",
+		and: "et",
+		better: "meilleur",
+		birthday: "anniversaire",
+		christmas: "Noël",
+		family: "la famille",
+		filled: "rempli",
+		well: "bien",
+		greeting: "salutations",
+		happy: "bonnes",
+		holidays: "vacances",
+		joy: "joie",
+		merry: "joyeux",
+		new: "nouveau",
+		presents: "cadeaux",
+		seasons: "saisons",
+		the: "le",
+		wishes: "voeux",
+		year: "année"
+	}
+	return {
+		translateToFrench: function(input) {
+			var output=[];
+			for(var word of input) {
+				output.push(french[word]);
+			}
+			return output;
+		}
+	}
+
+
+}());
+
