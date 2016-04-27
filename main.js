b78e0b488c2225af30bfc7966787a69a1fbdb00b
Adam Oswalt <adam.oswalt@gmail.com>
2016-04-27 13:40:00 -0500

    Separate speak function to fix Klingon

--- main.js
+++ main.js
@@ -11,26 +11,31 @@ document.getElementById("translate-button")
     switch(languageSelect.value) {
       case "lang-croatian":
         translationString = Translator.translateToCroatian(userInputArray).join(" ");
+        speak(translationString);
         textOutput.style.fontFamily = baseFontFamily;
         break;
       case "lang-french":
         translationString = Translator.translateToFrench(userInputArray).join(" ");
+        speak(translationString);
         textOutput.style.fontFamily = baseFontFamily ;
         break;
       case "lang-klingon":
         translationString = Translator.translateToKlingon(userInputArray).join(" ");
+        speak(translationString);
         translationString = Translator.toKlingonAlphabet(translationString);
         textOutput.style.fontFamily = "klingon";
         break;
     }
 
     textOutput.innerHTML = translationString;
+  });
 
-		window.speechSynthesis.speak(new SpeechSynthesisUtterance(""));
-		var msg = new SpeechSynthesisUtterance();
-		msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == 'Google UK English Female'; })[0];
-		msg.text = translationString;
-		speechSynthesis.speak(msg);
+function speak(text) {
+    window.speechSynthesis.speak(new SpeechSynthesisUtterance(""));
+    var msg = new SpeechSynthesisUtterance();
+    msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == 'Google UK English Female'; })[0];
+    msg.text = text;
+    speechSynthesis.speak(msg);
+}
 
-	});
 window.speechSynthesis.getVoices();
